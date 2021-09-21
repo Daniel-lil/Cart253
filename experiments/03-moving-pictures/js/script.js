722f4b3841724cb335b7ba36c8cc96e3278052c9
Daniel Little <Danswim02@gmail.com>
2021-09-21 11:56:55 -0400

    creation moving pictures

--- experiments/03-moving-pictures/js/script.js
+++ experiments/03-moving-pictures/js/script.js
@@ -6,6 +6,26 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0,
+};
+let circle1 = {
+  x:0,
+  y:250,
+  size:100,
+  fill:255,
+  alpha:200
+};
+let circle2 = {
+  x: 500,
+  y: 250,
+  size: 50,
+  fill: 255,
+  alpha: 200
+};
+
 "use strict";
 
 
@@ -21,7 +41,8 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+createCanvas(500,500);
+noStroke();
 }
 
 
@@ -29,5 +50,6 @@ function setup() {
 Description of draw()
 */
 function draw() {
-
+background(bg.r,bg.b,bg.b);
+bg.r = bg.r +1;
 }
