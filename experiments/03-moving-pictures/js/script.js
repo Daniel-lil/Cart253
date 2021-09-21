79e5a1173a21a6a24e0161ac6db68040ac167dd3
Daniel Little <Danswim02@gmail.com>
2021-09-21 16:39:40 -0400

    continued work on experiment 3

--- experiments/03-moving-pictures/js/script.js
+++ experiments/03-moving-pictures/js/script.js
@@ -51,5 +51,23 @@ Description of draw()
 */
 function draw() {
 background(bg.r,bg.b,bg.b);
-bg.r = bg.r +1;
+
+bg.b = map(mouseY, 0, height, 0, 255);
+
+
+//first move
+circle1.x = circle1.x + 1;
+circle1.x = constrain(circle1.x,0,width/2);
+circle1.size = circle1.size + 1.6
+circle1.size = constrain(circle1.size,0,width)
+fill(circle1.fill,circle1.alpha);
+ellipse(circle1.x,circle1.y,circle1.size);
+
+circle2.x = circle2.x + -1;
+circle2.x = constrain(circle2.x,width/2,width);
+circle2.size = circle1.size/4;
+fill(circle2.fill,circle2.alpha);
+ellipse(circle2.x,circle2.y,circle2.size);
+
+
 }
