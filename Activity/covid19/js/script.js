79260ff8db9c3305e14d27dd9e02188d09b5e164
Daniel Little <Danswim02@gmail.com>
2021-09-28 15:16:12 -0400

    Starting exercise dodge em

--- Activity/covid19/js/script.js
+++ Activity/covid19/js/script.js
@@ -15,8 +15,6 @@ let covid19 = {
 let user = {
   x: 250,
   y: 250,
-  size: 100,
-  fill: 255
 };
 
 
@@ -34,16 +32,13 @@ Description of draw()
 function draw() {
   background(0);
 
-covid19.x = covid19.x + covid19.vx;
-covid19.y = covid19.y + covid19.vy;
+covid19.x = covid19.x = covid19.vx;
+covid19.y = covid19.y = covid19.vy;
 
 if (covid19.x > width) {
   covid19.x = 0;
   covid19.y = random(0,height);
 }
-
-user.x = mouseX;
-user.y = mouseY;
 
 fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
 ellipse(covid19.x,covid19.y,covid19.size);
