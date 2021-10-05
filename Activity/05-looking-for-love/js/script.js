deafe7afdee66efe476ad1a8ed1c5deb57fed6a8
Daniel Little <Danswim02@gmail.com>
2021-10-05 17:18:37 -0400

    added jump scare and changed movement

--- Activity/05-looking-for-love/js/script.js
+++ Activity/05-looking-for-love/js/script.js
@@ -5,6 +5,8 @@ Daniel Little
 
 "use strict";
 
+let imgScary;
+
 let circle1 = {
   x: 0,
   y: 250,
@@ -25,7 +27,7 @@ let circle2 = {
   size: 100,
   vx: 0,
   vy: 0,
-  speed: 5,
+  speed: 2,
   fill: {
     r: 148,
     g: 7,
@@ -34,6 +36,10 @@ let circle2 = {
 };
 
 let state = `title`; // can be: title, simulation, love & sadness
+
+function preload() {
+  imgScary = loadImage ('assets/images/jumpScare.jpeg');
+}
 
 function setup() {
 createCanvas(500, 500);
@@ -60,26 +66,67 @@ if (state === `title`){
   title();
 }
 else if (state ===`simulation`){
-
+  simulation();
+}
+else if(state ===`love`){
+  love();
 }
 else if (state === `sadness`){
-
+  sadness();
+}
+else if (state === `jumpScare`) {
+  jumpScare();
 }
 
 function title(){
+  push();
   textSize(150);
   textStyle(BOLD);
   fill(200,10,60);
   textAlign(CENTER,CENTER);
   text(`love?`, width/2, height/2);
+  pop();
 }
 
 function simulation(){
-simulation();
 move();
 checkOffScreen();
 checkOverlap();
 display();
+handleInput();
+checkForScare();
+}
+
+function love(){
+  push();
+  textSize(150);
+  textStyle(BOLD);
+  fill(230,10,70);
+  textAlign(CENTER,CENTER);
+  text(`love!`, width/2, height/2);
+  pop();
+}
+
+function sadness(){
+  push();
+  textSize(115);
+  textStyle(BOLD);
+  fill(25,50,255);
+  textAlign(CENTER,CENTER);
+  text(`sadness`, width/2, height/2);
+  pop();
+}
+
+function jumpScare(){
+  push();
+  imageMode(CENTER)
+  image(imgScary,250,250,540,540);
+  textSize(75);
+  textStyle(BOLD);
+  fill(45,255,0);
+  textAlign(CENTER,CENTER);
+  text(`Love Is Scary`, width/2, height/2);
+  pop();
 }
 
 function move(){
@@ -89,20 +136,56 @@ circle1.y = circle1.y + circle1.vy;
 
 circle2.x = circle2.x + circle2.vx;
 circle2.y = circle2.y + circle2.vy;
+
+let change = random();
+if (change < 0.01) {
+  circle2.vx= random(-circle2.speed, circle2.speed);
+  circle2.vy= random(-circle2.speed, circle2.speed);
+  }
+}
+
+function handleInput() {
+  //allows user control of circle1 using keys
+  if (keyIsDown(LEFT_ARROW)){
+    circle1.vx = - circle1.speed;
+  }
+  else if (keyIsDown(RIGHT_ARROW)) {
+    circle1.vx = circle1.speed;
+  }
+  else {
+    circle1.vx = 0;
+  }
+
+if (keyIsDown(UP_ARROW)) {
+  circle1.vy = -circle1.speed;
+}
+  else if (keyIsDown(DOWN_ARROW)) {
+    circle1.vy = circle1.speed
+  }
+  else {
+    circle.vy = 0;
+  }
 }
 
 function checkOffScreen(){
-//check if circles have gone offscreen
+//check if circles have gone offscreen and triggers sad ending
 if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height )  {
-  //sad ending
+  state = `sadness`;
 }
 }
 
+function checkForScare() {
+  //check if circle is in corner and triggers jumpScare
+  if (circle1.x >= 425 & circle1.y >= 425) {
+    state = `jumpScare`;
+  }
+}
+
 function checkOverlap(){
-//check if circles overlap
+//check if circles overlap and triggers happy ending
 let d = dist(circle1.x,circle1.y,circle2.x,circle2.y);
 if (d < circle1.size/2 + circle2.size/2){
-  //Love ending
+  state = `love`;
 }
 }
 
@@ -117,4 +200,10 @@ function display(){
   ellipse(circle2.x,circle2.y,circle2.size);
   pop();
 }
+}
+
+function mousePressed(){
+  if (state === `title`){
+    state = `simulation`;
+  }
 }
