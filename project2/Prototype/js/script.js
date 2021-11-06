97f60c9b510895ed350c0db7333fe96f54b04982
Daniel Little <Danswim02@gmail.com>
2021-11-06 01:58:17 -0400

    Update script.js

--- project2/Prototype/js/script.js
+++ project2/Prototype/js/script.js
@@ -1,55 +1,58 @@
 /**
-Project 2 Prototype
+Prototype audio visualizer
 Daniel Little
 
-making an audio visualizer
-this is me trying out basic functions for it
+Very basic Prototype of my 2nd project and testing of using
+amplitude data from a song to modulate attributes of object
 */
 
 "use strict";
 let song;
-let volume;
-//declaring properties of circle
-let circle = {
-  size: 0,
-  fill: 0,
-};
+let amplitude;
+
 /**
-preloading song
+preload song
 */
 function preload() {
-  song = loadSound(`assets/sounds/Song.mp3`);
-}
-/**
-Description of setup
-*/
-function setup() {
-createCanvas(windowWidth,windowHeight);
-//sets background to black
-}
-/**
-Description of draw()
-*/
-function draw() {
-  //sets background to black
-background(0);
-//pause and play song
-  function mousePressed(){
-  userStartAudio();
-  playSong();
+song = loadSound(`assets/sounds/song.mp3`);
 }
 
-function playSong(){
-  if (song.isPlaying()){
-    song.pause();
-  }else {
-    song.play();
-  }
-  }
-push();
-//display circle
-fill(circle.fill);
+/**
+creates canvas, ability to play song and declares variable to store amplitude
+*/
+function setup() {
+let cnv = createCanvas(windowWidth,windowHeight);
+cnv.mouseClicked(playOrPauseSong);
+amplitude = new p5.Amplitude();
+}
+
+
+/**
+draws circles and pauses + plays song
+*/
+function draw() {
+background(0);
+
+//gets amplitude data and maps it to a bigger scale for the shapes
+let level = amplitude.getLevel();
+let size = map(level, 0, 1, 0, 4000);
+//draws shapes
+ellipse(windowWidth/3, windowHeight/3, size,);
+ellipse(windowWidth/3*2, windowHeight/3, size,);
+ellipse(windowWidth/3*2, windowHeight/3*2, size,);
+ellipse(windowWidth/3, windowHeight/3*2, size,);
+ellipse(windowWidth/2, windowHeight/2, size,);
+fill(0);
 stroke(255);
-ellipse(windowWidth/2,windowHeight/2,circle.size);
-pop();
+}
+
+//pauses + plays song
+function playOrPauseSong() {
+  if (song.isPlaying() ){
+    song.pause();
+  } else {
+    song.loop();
+                amplitude = new p5.Amplitude();
+                amplitude.setInput(song);
+  }
 }
