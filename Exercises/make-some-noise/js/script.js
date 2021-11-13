ba1ec3ee392ee0b5ca04e3e8f81492cc25829acd
Daniel Little <Danswim02@gmail.com>
2021-11-12 19:17:24 -0500

    initial synth created with polysynth

--- Exercises/make-some-noise/js/script.js
+++ Exercises/make-some-noise/js/script.js
@@ -8,6 +8,7 @@ author, and this description to match your project!
 
 "use strict";
 
+let synth;
 
 /**
 Description of preload
@@ -16,18 +17,47 @@ function preload() {
 
 }
 
-
 /**
 Description of setup
 */
 function setup() {
+createCanvas(windowWidth, windowHeight);
 
+synth = new p5.PolySynth();
+
+userStartAudio();
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+background(0);
 
+controlNotes();
+}
+
+function controlNotes() {
+if (keyIsDown(65)){
+  synth.noteAttack(`A3`,1,0,1);
+} else if (keyIsDown(83)){
+synth.noteAttack(`B3`,1,0,1);
+} else if (keyIsDown(68)){
+synth.noteAttack(`C3`,1,0,1);
+}  else if (keyIsDown(70)){
+synth.noteAttack(`D3`,1,0,1);
+}  else if (keyIsDown(71)){
+synth.noteAttack(`E3`,1,0,1);
+}  else if (keyIsDown(72)){
+synth.noteAttack(`F3`,1,0,1);
+}  else if (keyIsDown(74)){
+synth.noteAttack(`G3`,1,0,1);
+}  else if (keyIsDown(73)){
+synth.noteAttack(`G#3`,1,0,1);
+}  else if (keyIsDown(75)){
+synth.noteAttack(`A4`,1,0,1);
+}
+}
+function keyReleased(){
+  synth.noteRelease();
 }
