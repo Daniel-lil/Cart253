05fb0c5ab585b5009b122f630b378a9de97c224e
Daniel Little <Danswim02@gmail.com>
2021-11-23 01:12:19 -0500

    Exercise Progress report: starting to add 3d shapes
    added in translucent cubes that are displayed when a key is pressed

--- Exercises/project-progress-report/js/script.js
+++ Exercises/project-progress-report/js/script.js
@@ -11,6 +11,7 @@ let song;
 let fft;
 let ampLow;
 let ampHigh;
+let boxSize;
 
 
 /**
@@ -25,7 +26,7 @@ song = loadSound(`assets/sounds/Song.mp3`);
 Description of setup
 */
 function setup() {
-  let cnv = createCanvas(windowWidth,windowHeight);
+  let cnv = createCanvas(windowWidth,windowHeight, WEBGL);
   cnv.mouseClicked(playOrPauseSong);
   fft = new p5.FFT();
 }
@@ -37,22 +38,53 @@ Description of draw()
 function draw() {
 
 let levels = fft.analyze();
-ampLow = fft.getEnergy(30, 550);
-let circleSize = map(ampLow, 0, 255, 1, 1250);
+ampLow = fft.getEnergy(30, 500);
+let circleSize = map(ampLow, 0, 255, 1, 1275);
+let spinSpeed = map(ampLow, 0, 255, 0, 0.5);
 ampHigh = fft.getEnergy(600, 3000);
-let backgroundColour = map(ampHigh, 1, 255, -80, 200);
+let backgroundColour = map(ampHigh, 0, 255, -100, 255);
+let cubeColour = ampHigh;
 
 background(backgroundColour/10,backgroundColour/2,backgroundColour);
 
-//draws shapes
-ellipse(windowWidth/3, windowHeight/3, circleSize,);
-ellipse(windowWidth/3*2, windowHeight/3, circleSize,);
-ellipse(windowWidth/3*2, windowHeight/3*2, circleSize,);
-ellipse(windowWidth/3, windowHeight/3*2, circleSize,);
-ellipse(windowWidth/2, windowHeight/2, circleSize,);
+//draws Circles
+push();
 noFill();
 stroke(255);
+ellipse(0 + width/5.5, 0 +  height/5.5, circleSize,);
+ellipse(0 - width/5.5, 0 +  height/5.5, circleSize,);
+ellipse(0 - width/5.5, 0 -  height/5.5, circleSize,);
+ellipse(0 + width/5.5,0 - height/5.5, circleSize,);
+ellipse(0, 0, circleSize,);
+pop();
+
+if (keyIsDown (65)){
+push();
+boxSize = 0 +1;
+rotateZ(frameCount * 0.01);
+rotateX(frameCount * 0.01);
+rotateY(frameCount * 0.01);
+fill(cubeColour/5,cubeColour,cubeColour*2, 180);
+stroke(cubeColour/5,cubeColour,cubeColour*2);
+box(90 * boxSize, 90 * boxSize, 90 * boxSize,);
+
+noStroke();
+fill(cubeColour/5,cubeColour,cubeColour*2, 20);
+box(150 * boxSize, 90 * boxSize, 90 * boxSize,);
+
+box(30 * boxSize, 90 * boxSize, 90 * boxSize,);
+
+box(90 * boxSize, 150 * boxSize, 90 * boxSize,);
+
+box(90 * boxSize, 30 * boxSize, 90 * boxSize,);
+
+box(90 * boxSize, 90 * boxSize, 150 * boxSize,);
+
+box(90 * boxSize, 90 * boxSize, 30 * boxSize,);
+pop();
 }
+}
+
 
 //pauses + plays song
 function playOrPauseSong() {
